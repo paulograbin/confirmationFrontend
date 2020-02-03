6c82c3bdb02643738b0b4c78bb330e3825f50b3c
Paulo Gräbin <plgrabin@gmail.com>
2020-02-03 19:06:13 -0300

    Add guard to some routes, specially the home/events one

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,38 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
-import { CallbackComponent} from './components/callback/callback.component';
-import { AuthGuard } from './services/auth.guard';
-import { AdminComponent } from './components/admin/admin.component';
-import { HomeComponent } from './components/home/home.component';
-import { ViewUserComponent } from './components/view-registration/view-user.component';
+import {AdminComponent} from './components/admin/admin.component';
+import {HomeComponent} from './components/home/home.component';
+import {ViewUserComponent} from './components/view-registration/view-user.component';
 import {ListUsersComponent} from './components/list-users/list-users.component';
 import {ListEventsComponent} from './components/list-events/list-events.component';
+import {ViewEventComponent} from './components/view-event/view-event.component';
+import {LoginComponent} from './login/login.component';
+import {AuthGuard} from './services/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: ListEventsComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'events',
+    component: ListEventsComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   },
   {
     path: 'user/view/:id',
     component: ViewUserComponent
+    // canActivate: [AuthGuard]
+  },
+  {
+    path: 'event/view/:id',
+    component: ViewEventComponent
     // canActivate: [AuthGuard]
   },
   {
@@ -25,17 +41,9 @@ const routes: Routes = [
     // canActivate: [AuthGuard]
   },
   {
-    path: 'callback',
-    component: CallbackComponent
-  },
-  {
     path: 'users',
     component: ListUsersComponent
-  },
-  {
-    path: 'events',
-    component: ListEventsComponent
-  },
+  }
 ];
 
 @NgModule({
