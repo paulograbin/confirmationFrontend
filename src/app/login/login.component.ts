3bfd2d787f2f7b796e2d55d2d2397d165a092232
Paulo Gräbin <plgrabin@gmail.com>
2020-02-03 01:36:28 -0300

    Refactor on login to use angular reactive forms

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthService} from '../services/auth.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
     selector: 'app-login',
@@ -9,31 +10,48 @@ import {AuthService} from '../services/auth.service';
 })
 export class LoginComponent implements OnInit {
 
-    username = 'plgrabin';
-    password = 'aaa';
-    invalidLogin = true;
+    loginForm: FormGroup;
+    username = 'teste';
+    password = 'teste2';
+    invalidLogin = false;
     errorMessage = 'error message';
-    validMessage = 'valid message';
 
     constructor(private router: Router,
                 private authService: AuthService) {
     }
 
     ngOnInit() {
+        this.loginForm = new FormGroup({
+            login: new FormControl('', Validators.required),
+            senha: new FormControl('', Validators.required),
+        });
     }
 
     handleLogin() {
-        this.authService.login(this.username, this.password)
-            .subscribe(
-                data => {
-                    console.log(data);
-                    this.router.navigate(['events']);
-                    this.invalidLogin = false;
-                },
-                error => {
-                    console.log(error);
-                    this.invalidLogin = true;
-                }
-            );
+        console.log('vai login com ', this.username, this.password);
+        console.log('vai login com ', this.loginForm.value);
+
+        if (this.loginForm.valid) {
+            this.authService.login(this.username, this.password)
+                .subscribe(
+                    data => {
+                        console.log(data);
+                        this.router.navigate(['events']);
+                        this.invalidLogin = false;
+                    },
+                    error => {
+                        this.invalidLogin = true;
+
+                        if (error.status === 401 || error.status === 400) {
+                            this.errorMessage = 'Dados inválidos';
+                        } else {
+                            this.errorMessage = 'Ops, parece que o servidor não está respondendo';
+                        }
+                    }
+                );
+        } else {
+            this.invalidLogin = true;
+            this.errorMessage = 'Preencha os dados!';
+        }
     }
 }
