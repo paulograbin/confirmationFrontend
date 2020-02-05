7c4740d2602013ceb3d05a1d024768d6abe02e54
Paulo Gräbin <plgrabin@gmail.com>
2020-02-05 00:39:00 -0300

    List events completely redesigned

--- src/app/components/list-events/list-events.component.ts
+++ src/app/components/list-events/list-events.component.ts
@@ -1,31 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {EventServiceService} from '../../services/event-service.service';
+import {EventModel} from '../../model/eventModel';
+import {UserModel} from '../../model/userModel';
+import {UserService} from '../../services/user.service';
 
 @Component({
-  selector: 'app-list-events',
-  templateUrl: './list-events.component.html',
-  styleUrls: ['./list-events.component.css']
+    selector: 'app-list-events',
+    templateUrl: './list-events.component.html',
+    styleUrls: ['./list-events.component.css']
 })
 export class ListEventsComponent implements OnInit {
 
-  public events;
+    events: EventModel[];
+    loggedUser: UserModel;
 
-  constructor(private eventService: EventServiceService) { }
+    constructor(private eventService: EventServiceService,
+                private userService: UserService) {
+    }
 
-  ngOnInit() {
-    this.fetchAndDisplayEvents();
-  }
+    dayName = ['domingo', 'segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sábado'];
+    monName = ['janeiro', 'fevereiro', 'março', 'abril', 'Maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
 
-  private fetchAndDisplayEvents() {
-    this.eventService.getAllEvents().subscribe(
-      data => {
-        console.log(data);
 
-        this.events = data;
-      },
-      err => console.error(err),
-      () => console.log('events loaded')
-    );
+    ngOnInit() {
+        // console.log(this.dayName[date.getDay()] + ', dia ' + date.getDate() + ' de ' + this.monName[date.getMonth()]);
+        this.userService.fetchDetailsAboutLoggedUser().subscribe(
+            data => {
+                console.log('logged user', data);
+                this.loggedUser = data;
+            },
+            error => console.error(error),
+            () => this.fetchUserInvitations(this.loggedUser.id)
+        );
 
-  }
+
+    }
+
+    private fetchAndDisplayEvents() {
+        this.eventService.getAllEvents().subscribe(
+            data => {
+                console.log(data);
+
+                this.events = data;
+            },
+            err => console.error(err),
+            () => console.log('events loaded')
+        );
+    }
+
+    private fetchUserInvitations(id) {
+        this.eventService.getUserInvitations(id).subscribe(
+            data => {
+                console.log(data);
+
+                this.events = data;
+            },
+            err => console.error(err),
+            () => console.log('events loaded')
+        );
+    }
+
+    private confirmPresenceInEvent() {
+        this.eventService.confirmPresence(7, 1).subscribe();
+
+        this.ngOnInit();
+    }
 }
