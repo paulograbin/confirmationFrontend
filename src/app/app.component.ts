ec05ba3d991cf1fcbe1b80b55f504d6a772a1f08
Paulo Gräbin <plgrabin@gmail.com>
2020-02-05 00:38:10 -0300

    App component now does nothing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,6 @@ import { AuthService } from './services/auth.service';
 export class AppComponent {
   title = 'confirmationFrontend';
 
-  constructor(private authService: AuthService) {
-    authService.handleAuthentication();
+  constructor() {
   }
 }
