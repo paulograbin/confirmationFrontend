74d89da084ec9b3e778307925b87b5e65418ae93
Paulo Gräbin <plgrabin@gmail.com>
2020-02-08 21:10:42 -0300

    First use of UserModel interface just created. Use also some other methods from rxjs.

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,7 +2,9 @@ import {Injectable} from '@angular/core';
 
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {UserModel} from '../model/userModel';
-import {Observable} from 'rxjs';
+import {UserModelInterface} from '../model/userModelInteface';
+import {catchError, tap} from 'rxjs/operators';
+import {throwError} from 'rxjs';
 
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type': 'application/json'})
@@ -15,9 +17,12 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-
   fetchDetailsAboutLoggedUser() {
-    return this.http.get<UserModel>('/server/users/me');
+    return this.http.get<UserModelInterface>('/server/users/me')
+        .pipe(
+            tap(data => console.log(JSON.stringify(data))),
+            catchError(this.handleError)
+        );
   }
 
   getAllUsers() {
@@ -45,5 +50,17 @@ export class UserService {
     console.log('Activating user...');
 
     return this.http.put('/server/users/' + id + '/activate', null);
+  }
+
+  private handleError(err) {
+    let errorMessage: string;
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = `An error ocurred: ${err.error.message}`;
+    } else {
+      errorMessage = `Backend returned code ${err.status}: ${err.body.error}`;
+    }
+
+    console.error(err);
+    return throwError(errorMessage);
   }
 }
