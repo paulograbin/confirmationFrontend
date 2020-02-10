1f804463119a386788d2c789d758d4694ec97d19
Paulo Gräbin <plgrabin@gmail.com>
2020-02-10 00:20:28 -0300

    EventModel now implements EventInterface

--- src/app/model/eventModel.ts
+++ src/app/model/eventModel.ts
@@ -1,12 +1,34 @@
 import {ParticipationModel} from './participationModel';
+import {DateFormatter} from '../services/dateFormatter';
 
-export class EventModel {
+
+export interface EventInterface {
+
+    participants: ParticipationModel[];
+    id: string;
+    title: string;
+    dateTime: Date;
+    translatedDateTime: string;
+    creationDate: Date;
+    description: string;
+
+}
+
+export class EventModel implements EventInterface {
     constructor(
         public participants: ParticipationModel[],
         public id: string,
         public title: string,
-        public date: Date,
+        public dateTime: Date,
+        public translatedDateTime: string,
         public creationDate: Date,
-        public description: string) {
+        public description: string,
+        private dateFormatter: DateFormatter) {
+
+        this.translatedDateTime = this.dateFormatter.formatDate(new Date(dateTime));
+    }
+
+    getFormatedDate(): string {
+        return this.dateFormatter.formatDate(this.dateTime);
     }
 }
