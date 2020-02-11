200fe89be90f7ce88d816a3628398488965ddf51
Paulo Gräbin <plgrabin@gmail.com>
2020-02-10 23:39:20 -0300

    Import a bunch of stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,49 +1,58 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AccordionModule } from 'ngx-bootstrap/accordion';
-import { CallbackComponent } from './components/callback/callback.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AccordionModule} from 'ngx-bootstrap/accordion';
 
 
-import { AuthService } from './services/auth.service';
-import { AuthGuard } from './services/auth.guard';
-import { BikeService } from './services/bike.service';
-import { AdminComponent } from './components/admin/admin.component';
-import { HomeComponent } from './components/home/home.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { ViewUserComponent } from './components/view-registration/view-user.component';
-import { ListUsersComponent } from './components/list-users/list-users.component';
-import { ListEventsComponent } from './components/list-events/list-events.component';
+import {DateFormatter} from './services/dateFormatter';
+import {AuthService} from './services/auth.service';
+import {AuthGuard} from './services/auth.guard';
+import {BikeService} from './services/bike.service';
+import {AdminComponent} from './components/admin/admin.component';
+import {HomeComponent} from './components/home/home.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {FormsModule} from '@angular/forms';
+import {ViewUserComponent} from './components/view-registration/view-user.component';
+import {ListUsersComponent} from './components/list-users/list-users.component';
+import {ListEventsComponent} from './components/list-events/list-events.component';
 import {HttpInterceptorService} from './services/http-interceptor.service';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { ViewEventComponent } from './components/view-event/view-event.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {ViewEventComponent} from './components/view-event/view-event.component';
+import {LoginComponent} from './login/login.component';
+import {LogoutComponent} from './logout/logout.component';
+import {ListCreatedEventsComponent} from './components/list-created-events/list-created-events.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CallbackComponent,
-    AdminComponent,
-    HomeComponent,
-    ViewUserComponent,
-    ListUsersComponent,
-    ListEventsComponent,
-    HeaderComponent,
-    FooterComponent,
-    ViewEventComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    ReactiveFormsModule
-  ],
-  providers: [BikeService, AuthService, AuthGuard, { provide: HTTP_INTERCEPTORS, useClass: HttpInterceptorService, multi: true }],
-  bootstrap: [AppComponent],
-  exports: [AccordionModule]
+    declarations: [
+        AppComponent,
+        AdminComponent,
+        HomeComponent,
+        ViewUserComponent,
+        ListUsersComponent,
+        ListEventsComponent,
+        HeaderComponent,
+        FooterComponent,
+        ViewEventComponent,
+        LoginComponent,
+        LogoutComponent,
+        ListCreatedEventsComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule
+    ],
+    // tslint:disable-next-line:max-line-length
+    providers: [DateFormatter, BikeService, AuthService, AuthGuard, {provide: HTTP_INTERCEPTORS, useClass: HttpInterceptorService, multi: true}],
+    bootstrap: [AppComponent],
+    exports: [AccordionModule]
 })
-export class AppModule { }
+export class AppModule {
+}
