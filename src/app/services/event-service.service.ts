d96105357aac24ca11bbf09966ebc184c991b6e3
Paulo Gräbin <plgrabin@gmail.com>
2020-02-13 18:10:08 -0300

    If master tries to fetch event with id 0 the system identifies as an event creation and return a null object (https://en.wikipedia.org/wiki/Null_object_pattern)

--- src/app/services/event-service.service.ts
+++ src/app/services/event-service.service.ts
@@ -14,8 +14,48 @@ export class EventServiceService {
     return this.http.get<EventModel[]>('/server/events');
   }
 
-  getEvent(id: number) {
-    return this.http.get<EventModel>('/server/events/' + id);
+  getEvent(id: number): Observable<EventInterface> {
+    if (id === 0) {
+      return of(this.createNullEvent());
+    }
+
+    return this.http.get<EventInterface>('/server/events/' + id)
+        .pipe(
+            // tap(data => console.log('getEvent: ' + JSON.stringify(data))),
+            // tap(data => console.log('getEvent: ' + data)),
+            catchError(this.handleError)
+        );
+  }
+
+  private createNullEvent(): EventInterface {
+    console.log('Return null event');
+    return {
+      id: 0,
+      title: null,
+      creatorId: 0,
+      description: null,
+      creationDate: null,
+      dateTime: null,
+      participants: null,
+      translatedDateTime: null
+    };
+  }
+
+
+  private handleError(err) {
+    // in a real world app, we may send the server to some remote logging infrastructure
+    // instead of just logging it to the console
+    let errorMessage: string;
+    if (err.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      errorMessage = `Backend returned code ${err.status}: ${err.body.error}`;
+    }
+    console.error(err);
+    return throwError(errorMessage);
   }
 
   getUserInvitations(id: number) {
