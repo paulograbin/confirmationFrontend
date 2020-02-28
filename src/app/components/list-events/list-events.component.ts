9551267b608ed85ec4110f0145f00a0bfb4bf6eb
Paulo Gräbin <plgrabin@gmail.com>
2020-02-28 01:45:41 -0300

    Show confirmation message when confirming or rejecting participation

--- src/app/components/list-events/list-events.component.ts
+++ src/app/components/list-events/list-events.component.ts
@@ -1,8 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {EventServiceService} from '../../services/event-service.service';
-import {EventModel} from '../../model/eventModel';
 import {UserModel} from '../../model/userModel';
 import {UserService} from '../../services/user.service';
+import {DateFormatter} from '../../services/dateFormatter';
+import {ParticipationModel} from '../../model/participationModel';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
     selector: 'app-list-events',
@@ -11,49 +13,54 @@ import {UserService} from '../../services/user.service';
 })
 export class ListEventsComponent implements OnInit {
 
-    events: EventModel[];
+    participations: ParticipationModel[];
     loggedUser: UserModel;
 
+    confirmationMessage = '';
+    confirmed = false;
+
     constructor(private eventService: EventServiceService,
-                private userService: UserService) {
+                private userService: UserService,
+                private route: ActivatedRoute,
+                private dateFormatter: DateFormatter) {
     }
-
-    dayName = ['domingo', 'segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sábado'];
-    monName = ['janeiro', 'fevereiro', 'março', 'abril', 'Maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
-
 
     ngOnInit() {
-        // console.log(this.dayName[date.getDay()] + ', dia ' + date.getDate() + ' de ' + this.monName[date.getMonth()]);
-        this.userService.fetchDetailsAboutLoggedUser().subscribe(
+        this.route.data.subscribe(
             data => {
-                console.log('logged user', data);
-                this.loggedUser = data;
+                this.loggedUser = data.loggedUser;
             },
-            error => console.error(error),
-            () => this.fetchUserInvitations(this.loggedUser.id)
+            err => console.error(err)
         );
 
+        this.fetchEvents();
+}
 
+    private fetchEvents() {
+        // console.log('fetch events', this.loggedUser);
+
+        if (this.loggedUser.master) {
+            console.log('MC');
+            this.fetchEventsCreatedByUser(this.loggedUser.id);
+        } else {
+            console.log('Not MC');
+            this.fetchUserInvitations(this.loggedUser.id);
+        }
     }
 
-    private fetchAndDisplayEvents() {
-        this.eventService.getAllEvents().subscribe(
-            data => {
-                console.log(data);
-
-                this.events = data;
-            },
-            err => console.error(err),
-            () => console.log('events loaded')
-        );
+    private fetchEventsCreatedByUser(id) {
+        // this.eventService.getEventsCreatedByUser(id).subscribe(
+        //     data => {
+        //         this.participations = data;
+        //     }
+        // );
     }
 
     private fetchUserInvitations(id) {
         this.eventService.getUserInvitations(id).subscribe(
             data => {
-                console.log(data);
-
-                this.events = data;
+                this.participations = data;
+                console.log(this.participations);
             },
             err => console.error(err),
             () => console.log('events loaded')
