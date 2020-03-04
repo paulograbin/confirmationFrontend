ef7bec4d38a23a3fdad6da64d427197c6e46979d
Paulo Gräbin <plgrabin@gmail.com>
2020-03-04 00:22:20 -0300

    Making components not private, as indicated by running ng lint

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,7 +12,7 @@ export class HeaderComponent implements OnInit {
 
   loggedUser: UserInterface;
 
-  constructor(private authService: AuthService,
+  constructor(public authService: AuthService,
               private userService: UserService) { }
 
   ngOnInit() {
