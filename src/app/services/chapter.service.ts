f9dd2b4686c91eadca59e057c71a17e3e6952248
Paulo Grabin <plgrabin@gmail.com>
2020-06-21 23:49:52 -0300

    Create chapter service

---
+++ src/app/services/chapter.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import {Observable} from 'rxjs';
+import {UserInterface} from '../model/userModel';
+import {Chapter} from '../model/chapterModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChapterService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  backendUrl = environment.localApiAddress;
+  chapterServiceUrl = this.backendUrl + '/chapter';
+
+  fetchAllChapters(): Observable<Chapter[]> {
+    console.log('get all chapters from server');
+    return this.http.get<Chapter[]>(`${this.chapterServiceUrl}`);
+  }
+}
