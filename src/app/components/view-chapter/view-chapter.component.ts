270970f8e678cc827bd434dba16548bc32c417bd
Paulo Grabin <plgrabin@gmail.com>
2020-08-11 01:39:33 -0300

    Create View Chapter component

---
+++ src/app/components/view-chapter/view-chapter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-chapter',
+  templateUrl: './view-chapter.component.html',
+  styleUrls: ['./view-chapter.component.css']
+})
+export class ViewChapterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
