0634cda66d32e6371d448df744676891ab020563
Paulo Grabin <plgrabin@gmail.com>
2020-09-27 18:36:25 -0300

    Methods to read and convert user request

--- src/app/services/user-request.service.ts
+++ src/app/services/user-request.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
 import {Observable} from 'rxjs';
 import {UserRequestInterface} from '../model/userRequestInterface';
+import {UserInterface} from '../model/userModel';
 
 @Injectable({
     providedIn: 'root'
@@ -17,5 +18,9 @@ export class UserRequestService {
 
     getUserRequest(id: string): Observable<UserRequestInterface> {
         return this.http.get<UserRequestInterface>(`${this.userRequestUrl}/${id}`);
+    }
+
+    convertRequestToUser(requestId: string, convertUserRequest: any) {
+        return this.http.post<UserInterface>(`${this.userRequestUrl}/${requestId}`, convertUserRequest);
     }
 }
