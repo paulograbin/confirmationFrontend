b18c18dca846d8b1f56f54ffc1e99830f1b4561d
Paulo Grabin <plgrabin@gmail.com>
2020-09-27 17:59:53 -0300

    Include userRequestInterface.ts

---
+++ src/app/model/userRequestInterface.ts
@@ -0,0 +1,15 @@
+export interface UserRequestInterface {
+
+    requestId: string;
+    firstName: string;
+    lastName: string;
+    username: string;
+    email: string;
+    chapterId: string;
+    chapterName: string;
+    creationDate: Date;
+    expirationDate: Date;
+    conversionDate: Date;
+    successful: boolean;
+
+}
