83cff3ceebd912eb0df4d5bb92b028f00ec8f509
Paulo Grabin <plgrabin@gmail.com>
2021-04-24 16:17:04 -0300

    Include link for GCP backend.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
     production: true,
     // localApiAddress: '/server',
     localApiAddress: 'https://confirmation-prod.herokuapp.com'
+    // localApiAddress: 'http://35.239.198.50:3000'
 };
