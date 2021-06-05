ce12d97f9ae998e7afd538269ebca4ff248c4a78
Paulo Grabin <plgrabin@gmail.com>
2021-06-05 12:42:45 -0300

    Some small fixes to remove duplicated css files

--- src/app/components/forgot-password/forgot-password.component.ts
+++ src/app/components/forgot-password/forgot-password.component.ts
@@ -6,7 +6,7 @@ import {UserService} from '../../services/user.service';
 @Component({
     selector: 'app-forgot-password',
     templateUrl: './forgot-password.component.html',
-    styleUrls: ['./forgot-password.component.css']
+    styleUrls: ['../../login/login.component.css']
 })
 export class ForgotPasswordComponent implements OnInit {
 
