2f67ff6e040ec7c9fa33156a3b3c05e3e5c78f5c
Paulo Grabin <plgrabin@gmail.com>
2021-09-12 22:31:36 -0300

    Include alert components in root app. Remove empty css file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import {Component} from '@angular/core';
+import {UserService} from './services/user.service';
+import {UserInterface} from './model/userModel';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'confirmationFrontend';
 
-  constructor() {
-  }
+    // user: UserInterface;
+
+    constructor(private userService: UserService) {
+        // userService.fetchDetailsAboutLoggedUser()
+        //     .subscribe(data => {
+        //         console.log(data);
+        //         this.user = data;
+        //     });
+    }
 }
