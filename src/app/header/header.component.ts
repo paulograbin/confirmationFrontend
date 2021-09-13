bfa86a098adbd4de6eaca28dfe91537ea6cc2547
Paulo Grabin <plgrabin@gmail.com>
2021-09-12 23:09:16 -0300

    Remove code from header.component.ts

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,35 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../services/auth.service';
 import {UserInterface} from '../model/userModel';
 import {UserService} from '../services/user.service';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
 
-  loggedUser: UserInterface;
+    loggedUser: UserInterface;
 
-  constructor(public authService: AuthService,
-              private userService: UserService) { }
+    constructor(public authService: AuthService,
+                private userService: UserService) {}
 
-  ngOnInit() {
-    this.userService.fetchDetailsAboutLoggedUser().subscribe(
-        data => {
-          // console.log('logged user', data);
-          this.loggedUser = data;
-        },
-        error => console.error(error),
-        () => {
-          // if (this.loggedUser.master) {
-          //   console.log('MC');
-          // } else {
-          //   console.log('Not MC');
-          // }
-        }
-    );
-  }
-
+    ngOnInit() {
+        this.userService.fetchDetailsAboutLoggedUser()
+            .subscribe(
+                data => {
+                    // console.log('logged user', data);
+                    this.loggedUser = data;
+                },
+            );
+    }
 }
