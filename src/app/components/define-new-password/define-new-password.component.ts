a60b82413b06fe2a3d439ffa9a403ed9eccc7e0e
Paulo Grabin <plgrabin@gmail.com>
2021-09-12 23:17:53 -0300

    Implement new component for setting new password

--- src/app/components/define-new-password/define-new-password.component.ts
+++ src/app/components/define-new-password/define-new-password.component.ts
@@ -1,76 +1,114 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {PasswordRequest} from '../../model/passwordRequest';
+import {first} from 'rxjs/operators';
+import {AlertService} from '../../services/alert/alert.service';
 import {PasswordResetService} from '../../services/password-reset.service';
+
+
+enum TokenStatus {
+    Validating,
+    Valid,
+    Invalid
+}
 
 @Component({
     selector: 'app-define-new-password',
     templateUrl: './define-new-password.component.html',
-    styleUrls: ['../meusdados/meusdados.component.css']
+    styleUrls: ['../../login/login.component.css']
 })
 export class DefineNewPasswordComponent implements OnInit {
 
-    passwordRequest: PasswordRequest;
-    passwordForm: FormGroup;
+    TokenStatus = TokenStatus;
+    tokenStatus = TokenStatus.Validating;
+    passwordRequestToken = null;
+    form: FormGroup;
+    loading = false;
+    submitted = false;
+    private passwordRequest: any;
 
-    confirmed = false;
-    confirmationMessage = '';
+    errorMessage = '';
+    successMessage = '';
 
 
-    constructor(private route: ActivatedRoute,
-                private formBuilder: FormBuilder,
-                private userService: PasswordResetService) {
-    }
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private passwordResetService: PasswordResetService,
+        private alertService: AlertService
+    ) {}
 
-    ngOnInit(): void {
+    ngOnInit() {
         this.route.data.subscribe(
             data => {
                 this.passwordRequest = data.passwordRequestRequest;
 
                 if (this.passwordRequest.successful) {
-                    console.log('Valid');
+                    this.passwordRequestToken = this.passwordRequest.requestCode;
+                    this.tokenStatus = TokenStatus.Valid;
                 } else {
-                    console.log('Invalid');
+                    this.tokenStatus = TokenStatus.Invalid;
                 }
             }
         );
 
-        this.passwordForm = this.formBuilder.group({
-            password: ['', [Validators.required,
-                Validators.minLength(6),
-                Validators.maxLength(128)]],
-            passwordConfirmation: ['', [Validators.required,
-                Validators.minLength(6),
-                Validators.maxLength(128)]],
+        this.form = this.formBuilder.group({
+            password: ['', [Validators.required, Validators.minLength(6)]],
+            confirmPassword: ['', Validators.required],
         });
+
+          // remove token from url to prevent http referer leakage
+        this.router.navigate([], {relativeTo: this.route, replaceUrl: true});
     }
 
     isValid() {
-        return this.passwordForm.valid &&
-            this.passwordForm.get('password').value === this.passwordForm.get('passwordConfirmation').value;
+        return this.form.valid &&
+            this.form.get('password').value === this.form.get('confirmPassword').value;
     }
 
-    changePassword(): void {
-        const passwordUpdateRequest = {
+    // convenience getter for easy access to form fields
+    get f() {
+        return this.form.controls;
+    }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // reset alerts on submit
+        // this.alertService.clear();
+
+        // stop here if form is invalid
+        if (this.form.invalid) {
+            return;
+        }
+
+        this.loading = true;
+
+        const newPasswordRequest = {
             requestCode: this.passwordRequest.requestCode,
-            password: this.passwordForm.get('password').value
+            password: this.f.password.value,
         };
 
-        this.userService.defineNewPasswordAfterForgot(passwordUpdateRequest)
-            .subscribe(
-                data => {
-                    console.log(data);
+        this.passwordResetService.defineNewPasswordAfterForgot(newPasswordRequest)
+            .pipe(first())
+            .subscribe({
+                next: () => {
+                    this.emptyFields();
+                    this.successMessage = 'Sua senha foi redefinida com sucesso, clique aqui para logar novamente ';
+
+                    this.alertService.success('Password reset successful, you can now login', {keepAfterRouteChange: true});
+                    // this.router.navigate(['/login'], {relativeTo: this.route});
                 },
-                error => {
-                    this.confirmed = true;
-                    this.confirmationMessage = 'Ocorreu algum problema na troca da senha';
-                },
-                () => {
-                    setTimeout(function() {
-                        this.confirmed = false;
-                    }.bind(this), 3000);
+                error: error => {
+                    // this.alertService.error(error);
+                    this.loading = false;
+                    this.errorMessage = 'Não conseguimos mudar a sua senha, tente novamente';
                 }
-            );
+            });
+    }
+
+    emptyFields() {
+        this.form.reset();
     }
 }
