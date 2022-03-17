a87579d808ea61f43f11b9eb7f42341ee9a96c1e
Paulo Grolli Grabin <paulograbin@gmail.com>
2022-03-17 14:56:52 -0300

    Publish new api endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     // localApiAddress: '/server',
     // localApiAddress: 'https://confirmation-prod.herokuapp.com'
-    localApiAddress: 'https://confirmation-docker-dw642nr2oq-rj.a.run.app'
+    localApiAddress: 'https://confirmation-prod.herokuapp.com'
 };
